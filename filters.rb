67f1976a0002ca1f9a5d5d0bdae93aac4714cde7
Casandra Normand <casandra.normand@outlook.com>
2024-02-28 18:53:32 -0500

    feat: finding candidates by id

--- filters.rb
+++ filters.rb
@@ -2,7 +2,11 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  candidates.each do |candidate|
+    return candidate if candidate[:id] == id
+  end
+  # Return nil if no candidate with the given id is found
+  return nil
   end
   
   def experienced?(candidate)

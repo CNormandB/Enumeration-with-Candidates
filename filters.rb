876115772a257aa89cd77d24b623038b6a7e74d2
Casandra Normand <casandra.normand@outlook.com>
2024-02-28 19:04:12 -0500

    feat: filter to find the qualified candidates

--- filters.rb
+++ filters.rb
@@ -1,24 +1,51 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
+#finds candidate by id
 def find(id)
   candidates.each do |candidate|
     return candidate if candidate[:id] == id
   end
   # Return nil if no candidate with the given id is found
   return nil
-  end
+end
   
-  def experienced?(candidate)
+#determines if the candidate has 2+ years of experience
+def experienced?(candidate)
   if candidate.has_key?(:years_of_experience) && candidate[:years_of_experience] >= 2
     return true
   else
     return false
   end
-  end
+end
   
-  def qualified_candidates(candidates)
-    # Your code Here
+#dertermines is the candidate is qualified
+def qualified_candidates(candidates)
+   candidates.select do |candidate|
+    experienced?(candidate) &&
+    has_enough_github_points?(candidate) &&
+    knows_ruby_or_python?(candidate) &&
+    applied_recently?(candidate) &&
+    is_adult?(candidate)
   end
-  
-  # More methods will go below
+end
+
+#checks the candidate's github points
+def has_enough_github_points?(candidate)
+  candidate[:github_points] && candidate[:github_points] >= 100
+end
+
+#checks if candidate knows ruby or python
+def knows_ruby_or_python?(candidate)
+  candidate[:languages] && (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python'))
+end
+
+#checks the date of the aplication and makes sure it's recent
+def applied_recently?(candidate)
+  candidate[:date_applied] && (Date.today - candidate[:date_applied]).to_i <= 15
+end
+
+#makes sure the candidate is an adult
+def is_adult?(candidate)
+  candidate[:age] && candidate[:age] > 17
+end
